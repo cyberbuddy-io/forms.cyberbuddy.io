f05dfaf27a440d25f5b17879864114162f661b41
cyberbuddy-manas <rspmanas1@gmail.com>
2022-09-06 00:42:53 +0530

    adding forms for cess event

---
+++ CESS/CESS-Scavenger-Hunt-3-3/app.js
@@ -0,0 +1,165 @@
+let date = new Date();
+
+// document.getElementById('form-1').style.display = 'none';
+
+function start() {
+  var ele = document.querySelectorAll("input");
+  ele.forEach((e) => {
+    e.disabled = true;
+    e.placeholder = "This form is closed.";
+  });
+};
+
+function snackbar() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.classList.remove("show"); }, 3000);
+}
+
+function send_message() {
+  var name1 = document.getElementById("name");
+  var bin = 0;
+
+  var fields = [name1];
+  for (var i = 0; i < fields.length ; i++) {
+    if (fields[i].value == '') {
+      fields[i].focus();
+      snackbar();
+      break;
+    }
+  }
+
+  if (name1.value != '') {
+    bin = 1;
+  }
+
+  if (bin == 1) {
+    document.getElementById("submit_btn").disabled = true;
+    document.getElementById("submit_btn").innerHTML = "Loading <i style='margin-left:8px' class='fa fa-spinner spinner'></i>";
+    var db = firebase.database();
+    db.ref("cess/scavenger3-3").push({
+      name1: name1.value,
+    })
+    .then( (onResolved) => {
+      document.getElementById("form-1").style.display = "none";
+      document.getElementById("form-2").style.display = "unset";
+      document.getElementById("username").innerHTML = name1.value;
+      window.scrollTo(0,0);
+    });
+  }
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function upload_check() {
+  var usr = document.getElementById("name");
+  if (usr.value == "") {
+    $("#resume").bind("click", preventDefault);
+    usr.focus();
+    snackbar();
+  } else {
+    $("#resume").unbind("click", preventDefault);
+  }
+}
+
+function upload_file() {
+  const ref = firebase.storage().ref("gfg-volunteer/");
+  const file = document.querySelector('#resume').files[0];
+  document.getElementById("file-name").innerHTML = file.name;
+  var usr = document.getElementById("name");
+  const name = (+new Date()) + '-' + usr.value;
+  const metadata = {
+    contentType: file.type
+  };
+  const task = ref.child(name).put(file, metadata);
+
+  document.getElementById("file-upload").innerHTML = "Please Wait <i class='fa fa-spinner spinner'></i>";
+  task
+    .then(snapshot => {
+      document.getElementById("file-upload").innerHTML = "File Uploaded!!";
+    })
+    .catch(console.error);
+}
+
+function white_bg() {   //to reset the colors
+  document.getElementById("test").classList = null;
+  var divs = document.querySelectorAll('#div-test');
+  divs.forEach((e) => {
+    e.classList = null;
+  });
+  var btns = document.querySelectorAll('#input-test');
+  btns.forEach((e) => {
+    e.classList = null;
+  });
+}
+
+function green_bg() {
+  white_bg();
+  document.getElementById("test").classList.add("green-bg");
+  var divs = document.querySelectorAll('#div-test');
+  divs.forEach((e) => {
+    e.classList.add("green-bg-div");
+  });
+  var btns = document.querySelectorAll('#input-test');
+  btns.forEach((e) => {
+    e.classList.add("green-bg-input");
+  });
+}
+
+function dark_bg() {
+  white_bg();
+  document.getElementById("test").classList.add("dark-bg");
+  var divs = document.querySelectorAll('#div-test');
+  divs.forEach((e) => {
+    e.classList.add("dark-bg-div");
+  });
+  var btns = document.querySelectorAll('#input-test');
+  btns.forEach((e) => {
+    e.classList.add("dark-bg-input");
+  });
+}
+
+function peach_bg() {
+  white_bg();
+  document.getElementById("test").classList.add("peach-bg");
+  var divs = document.querySelectorAll('#div-test');
+  divs.forEach((e) => {
+    e.classList.add("peach-bg-div");
+  });
+  var btns = document.querySelectorAll('#input-test');
+  btns.forEach((e) => {
+    e.classList.add("peach-bg-input");
+  });
+}
+
+function purple_bg() {
+  white_bg();
+  document.getElementById("test").classList.add("purple-bg");
+  var divs = document.querySelectorAll('#div-test');
+  divs.forEach((e) => {
+    e.classList.add("purple-bg-div");
+  });
+  var btns = document.querySelectorAll('#input-test');
+  btns.forEach((e) => {
+    e.classList.add("purple-bg-input");
+  });
+}
+
+$( "aside img" ).click(function() {
+  /*alert($( this ).css( "transform" ));*/
+  if (  $( this ).css( "transform" ) == 'none' ){
+      $(this).css("transform","rotate(180deg)");
+  } else {
+      $(this).css("transform","" );
+  }
+});
+
+function modal_dis() {
+  document.getElementById("modal").style.display = "none";
+}
+
+function modal() {
+  document.getElementById("modal").style.display = "grid";
+}
