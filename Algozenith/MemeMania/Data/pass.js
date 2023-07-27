7020f50afa0566619bb45f21e2446b6d80d82bbb
Manas Gupta <rspmanas1@gmail.com>
2023-07-27 16:29:37 +0530

    Update pass.js

--- Algozenith/MemeMania/Data/pass.js
+++ Algozenith/MemeMania/Data/pass.js
@@ -10,7 +10,7 @@ function passCheck() {
 	var pass = document.getElementById("pass");
 	var snack = document.getElementById("snackbar");
 
-	if (pass.value == "gndu@algozen") {
+	if (pass.value == "gndu@algozenith") {
 		snack.innerHTML = "Login Successful";
 		snackbar();
 		document.getElementById("login").style.display = "none";
