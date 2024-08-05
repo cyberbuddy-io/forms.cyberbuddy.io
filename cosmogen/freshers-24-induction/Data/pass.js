376378414ae8bb67911d956607ec83d20479b8c2
cyberbuddy-manas <rspmanas1@gmail.com>
2024-08-05 20:05:43 +0530

    adding changes

--- cosmogen/freshers-24-induction/Data/pass.js
+++ cosmogen/freshers-24-induction/Data/pass.js
@@ -10,7 +10,7 @@ function passCheck() {
 	var pass = document.getElementById("pass");
 	var snack = document.getElementById("snackbar");
 
-	if (pass.value == "statusbrew@123") {
+	if (pass.value == "cosmogen@123") {
 		snack.innerHTML = "Login Successful";
 		snackbar();
 		document.getElementById("login").style.display = "none";
